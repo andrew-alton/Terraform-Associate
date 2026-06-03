8c7bfe2507386b1161e24aa0e7860ee616d58cc4
Andrew Alton <122839430+andrew-alton@users.noreply.github.com>
2026-06-02 20:35:11 -0400

    Add subnet_id to ec2-instance module
    Added subnet_id parameter to EC2 instance module.

--- Chapter6- Terraform Modules & Workspaces/01- Terraform Modules/ec2-module.tf
+++ Chapter6- Terraform Modules & Workspaces/01- Terraform Modules/ec2-module.tf
@@ -1,4 +1,5 @@
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "6.0.2"
+  subnet_id = "subnet-0e4168177a9a93bc1" #put your VPC subnet of your region in your AWS Account
 }

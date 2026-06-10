b1a4c92a25619b1297a13cd6d747f1d66d6c6253
Andrew Alton <halavehzadeh@solutionsmetrix.com>
2026-06-09 23:21:27 -0400

    update:Terraform Workspace code

--- Chapter6- Terraform Modules & Workspaces/13- Overview of Terraform Workspace/workspace.tf
+++ Chapter6- Terraform Modules & Workspaces/13- Overview of Terraform Workspace/workspace.tf
@@ -9,4 +9,8 @@ locals {
 resource "aws_instance" "myec2" {
   ami           = "ami-08a0d1e16fc3f61ea"
   instance_type = local.instance_type[terraform.workspace]
+  tags = {
+    Name = "workspace-prod"
+  }
+
 }
